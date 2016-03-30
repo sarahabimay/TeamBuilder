54cd4d4e9912156faec6ce0bfe86a54ee3228c3f
sarahabimay <sarahabimay@gmail.com>
2016-03-30 11:18:04 +0100

    Move application module out of TeamBuilder::main() function and into TeamBuilderApp

--- lib/team_builder.ex
+++ lib/team_builder.ex
@@ -1,47 +1,9 @@
 defmodule TeamBuilder do
-  alias TeamBuilder.Members
   alias TeamBuilder.ConsoleWriter
   alias TeamBuilder.ConsoleReader
-  alias TeamBuilder.Team
+  alias TeamBuilder.TeamBuilderApp
 
   def main(_) do
-    ConsoleWriter.clear_screen()
-    ConsoleWriter.welcome_message()
-    start_application([], ConsoleReader, ConsoleWriter)
+    TeamBuilderApp.start_application([], ConsoleReader, ConsoleWriter)
   end
-
-  def start_application(members, display_reader, display_writer) do
-    prompt_for_command(members, display_reader, display_writer)
-  end
-
-  def prompt_for_command(members, display_reader, display_writer) do
-    display_reader
-    |> next_command
-    |> process_command(members, display_reader, display_writer)
-  end
-
-  defp process_command(:quit, _, _, display_writer), do: display_writer.goodbye_message()
-
-  defp process_command(:build_teams, members, display_reader, display_writer) do
-    get_team_type()
-    |> build_teams(members, display_reader, display_writer)
-    prompt_for_command(members, display_reader, display_writer)
-  end
-
-  defp process_command(new_member, members, display_reader, display_writer) do
-    members
-    |> Members.add_to_members(new_member)
-    |> prompt_for_command(display_reader, display_writer)
-  end
-
-  defp build_teams(team_type, members, _, display_writer) do
-    ConsoleWriter.clear_screen()
-    team_type
-    |> Team.allocate_members(members)
-    |> display_writer.display_teams
-  end
-
-  defp get_team_type(), do: %{:team_type => :fixed, :options => 4}
-
-  defp next_command(display_reader), do: display_reader.next_command
 end
---
+++ test/team_builder_app_test.exs
@@ -0,0 +1,67 @@
+defmodule TeamBuilderAppTest do
+  use ExUnit.Case
+  import ExUnit.CaptureIO
+  alias TeamBuilder.ConsoleReader
+  alias TeamBuilder.ConsoleWriter
+  alias TeamBuilder.TeamBuilderApp
+  doctest TeamBuilder
+
+  @team_type %{:team_type => :fixed, :options => 4}
+
+  defmodule FakeReader do
+   def next_command() do
+     "q"
+   end
+  end
+
+  defmodule FakeWriter do
+    def display_teams(teams) do
+      teams
+    end
+
+    def goodbye_message() do
+      "GoodBye"
+    end
+  end
+
+  test "typing q, closes the application" do
+    assert TeamBuilderApp.prompt_for_command([], FakeReader, FakeWriter) == "GoodBye"
+  end
+
+  test "typing b, builds the 4 empty teams" do
+    input = "b\nq\n"
+    expected_result = "[ Team 1 ]\n\n" <>
+                      "[ Team 2 ]\n\n" <>
+                      "[ Team 3 ]\n\n" <>
+                      "[ Team 4 ]\n\n" <>
+                      "Thank you for using TeamBuilder.\n"
+    result = capture_io([input: input, capture_prompt: false], fn() ->
+      IO.write TeamBuilderApp.prompt_for_command([], ConsoleReader, ConsoleWriter)
+    end)
+    assert String.contains?(result, expected_result)
+  end
+
+  test "consecutive new members redistributed across teams" do
+    input = "Sarah\nJames\nb\nq\n"
+    expected_result = "[ Team 1 ]\n" <>
+                       "[1] Sarah\n\n" <>
+                       "[ Team 2 ]\n" <>
+                       "[1] James\n\n" <>
+                       "[ Team 3 ]\n\n" <>
+                       "[ Team 4 ]\n\n" <>
+                       "Thank you for using TeamBuilder.\n"
+    result = capture_io([input: input, capture_prompt: false], fn() ->
+      IO.write TeamBuilderApp.prompt_for_command([], ConsoleReader, ConsoleWriter)
+    end)
+    assert String.contains?(result, expected_result)
+  end
+
+  def get_teams(4) do
+    [
+      %{:team => 1, :names => []},
+      %{:team => 2, :names => []},
+      %{:team => 3, :names => []},
+      %{:team => 4, :names => []},
+    ]
+  end
+end
