5ea4176bfbe6fa84e68814be413db7ab37d74702
sarahabimay <sarahabimay@gmail.com>
2016-04-07 18:26:34 +0100

    Introduce Console module to abstract away the write and read functions
    @maikon
    @jsuchy

--- lib/team_builder_app.ex
+++ lib/team_builder_app.ex
@@ -3,46 +3,45 @@ defmodule TeamBuilder.TeamBuilderApp do
   alias TeamBuilder.TeamTypeOptions
   alias TeamBuilder.Commands
 
-  def start_application(members, display_reader, display_writer, seed_state) do
-    display_writer.clear_screen()
-    display_writer.welcome_message()
-    prompt_for_command(members, display_reader, display_writer, seed_state)
+  def start_application(members, display, seed_state) do
+    display.welcome_message()
+    prompt_for_command(members, display, seed_state)
   end
 
-  def prompt_for_command(members, display_reader, display_writer, seed_state) do
-    display_reader
+  def prompt_for_command(members, display, seed_state) do
+    display
     |> next_command
-    |> process_command(members, display_reader, display_writer, seed_state)
+    |> process_command(members, display, seed_state)
   end
 
-  defp process_command(:quit, _, _, display_writer, _), do: display_writer.goodbye_message()
+  defp process_command(:quit, _, display, _), do: display.goodbye_message()
 
-  defp process_command(:build_teams, members, display_reader, display_writer, seed_state) do
-    display_writer.clear_screen()
-    {teams, next_seed_state} = build_teams(members, display_reader, seed_state)
-    display_writer.display_teams(teams)
-    prompt_for_command(members, display_reader, display_writer, next_seed_state)
+  defp process_command(:build_teams, members, display, seed_state) do
+    display.clear_screen()
+    {teams, next_seed_state} = build_teams(members, display, seed_state)
+    display.display_teams(teams)
+    prompt_for_command(members, display, next_seed_state)
   end
 
-  defp process_command({:add_member, new_member}, members, display_reader, display_writer, seed_state) do
-    display_writer.clear_screen()
+  defp process_command({:add_member, new_member}, members, display, seed_state) do
+    display.clear_screen()
     all_members = Members.add_to_members(members, new_member)
-    display_writer.display_members(all_members)
-    prompt_for_command(all_members, display_reader, display_writer, seed_state)
+    display.display_members(all_members)
+    prompt_for_command(all_members, display, seed_state)
   end
 
-  defp build_teams(members, display_reader, seed_state) do
-    get_team_type(display_reader)
+  defp build_teams(members, display, seed_state) do
+    get_team_type(display)
     |> Members.allocate_teams(members, seed_state)
   end
 
-  defp get_team_type(display_reader) do
-    display_reader.team_type_options()
+  defp get_team_type(display) do
+    display.team_type_options()
     |> TeamTypeOptions.get_team_type
   end
 
-  defp next_command(display_reader) do
-    display_reader.next_command
+  defp next_command(display) do
+    display.next_command
     |> Commands.command_type
   end
 end
