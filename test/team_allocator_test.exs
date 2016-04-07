e2e01ff391b29ece0755747cee0db52a9e7cf1ee
sarahabimay <sarahabimay@gmail.com>
2016-04-07 14:30:52 +0100

    Introduce TestHelper functions to MembersTest and TeamAllocatorTest

--- test/team_allocator_test.exs
+++ test/team_allocator_test.exs
@@ -5,10 +5,10 @@ defmodule TeamAllocatorTest do
 
   test "select 4 members randomly" do
     number_of_members = 4
-    members = generate_members(number_of_members)
+    members = TestHelper.generate_members(number_of_members)
     seed_state = :rand.export_seed_s(:rand.seed(:exsplus))
     selected = Enum.take_random(members, number_of_members)
-    expected = {selected, remaining_members(selected, members), :rand.export_seed()}
+    expected = {selected, TestHelper.remaining_members(selected, members), :rand.export_seed()}
     assert TeamAllocator.members_selection(members, number_of_members, seed_state) == expected
   end
 
@@ -27,14 +27,5 @@ defmodule TeamAllocatorTest do
     teams_and_seed = [member1, member2, random_seed]
     expected = {[member1, member2], random_seed[:new_seed]}
     assert TeamAllocator.separate_teams_and_seed(teams_and_seed) == expected
-  end
-
-  def generate_members(number) do
-    Enum.map(1..number, fn(num) -> "name#{num}" end)
-  end
-
-  def remaining_members(selected_members, all_members) do
-    {_, remaining} = Enum.partition(all_members, fn(x) -> Enum.any?(selected_members, fn(s) -> s == x end) end)
-    remaining
   end
 end
